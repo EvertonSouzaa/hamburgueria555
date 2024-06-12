e347b32963ed4690bd69bc853cdbc3f9b2debc0d
Everton <everton.s.dev@outlook.com>
2024-06-12 18:35:56 -0300

    ajustar login

--- script.js
+++ script.js
@@ -1,60 +1,261 @@
-// function sendToWhatsApp(product, price) {
-//   const phoneNumber = "5521979026552"; // Substitua pelo número de telefone desejado no formato internacional
-//   const message = `Olá, gostaria de mais informações sobre o produto ${product} que custa ${price}.`;
-//   const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
-//     message
-//   )}`;
-
-//   window.open(whatsappURL, "_blank");
-// }
-
-let total = 0;
 let cart = [];
 
-function increaseQuantity(id) {
-  const quantityInput = document.getElementById(id);
-  quantityInput.value = parseInt(quantityInput.value) + 1;
+/**
+ * Função para efetuar login do usuário
+ */
+function login() {
+  const name = document.getElementById("login-name").value;
+  const address = document.getElementById("login-address").value;
+  const phone = document.getElementById("login-phone").value;
+  const observations = document.getElementById("login-observations").value;
+
+  if (!name || !address || !phone) {
+    alert("Por favor, preencha Nome, Telefone e Endereço corretamente.");
+    return;
+  }
+
+  // Armazenar informações do usuário no localStorage
+  localStorage.setItem("name", name);
+  localStorage.setItem("address", address);
+  localStorage.setItem("phone", phone);
+  localStorage.setItem("observations", observations);
+
+  showUserInfo();
+  alert(`Bem-vindo, ${name}! Você está conectado.`);
+  closeLoginPopup();
+  window.location.href = "#cardapio"; // Redirecionar para o cardápio
 }
 
-function decreaseQuantity(id) {
-  const quantityInput = document.getElementById(id);
-  if (quantityInput.value > 1) {
-    quantityInput.value = parseInt(quantityInput.value) - 1;
+/**
+ * Função para exibir as informações do usuário
+ */
+function showUserInfo() {
+  const name = localStorage.getItem("name");
+  const loginSection = document.getElementById("login-section");
+  const userInfoSection = document.getElementById("user-info-section");
+  const userNameSpan = document.getElementById("user-name");
+
+  if (name) {
+    userNameSpan.textContent = name;
+    loginSection.style.display = "none";
+    userInfoSection.style.display = "block";
+  } else {
+    loginSection.style.display = "block";
+    userInfoSection.style.display = "none";
   }
 }
 
-function addToCart(productName, productPrice, quantityId) {
-  const quantity = parseInt(document.getElementById(quantityId).value);
-  const itemTotal = productPrice * quantity;
-  total += itemTotal;
-  // document.getElementById("total").innerText = total
-  //   .toFixed(2)
-  //   .replace(".", ",");
-  // alert(
-  //   `${productName} adicionado ao carrinho. Quantidade: ${quantity}, Total: R$ ${itemTotal
-  //     .toFixed(2)
-  //     .replace(".", ",")}`
-  // );
+/**
+ * Função para preencher os campos de login com as informações armazenadas
+ */
+function populateLoginFields() {
+  document.getElementById("login-name").value =
+    localStorage.getItem("name") || "";
+  document.getElementById("login-address").value =
+    localStorage.getItem("address") || "";
+  document.getElementById("login-phone").value =
+    localStorage.getItem("phone") || "";
+  document.getElementById("login-observations").value =
+    localStorage.getItem("observations") || "";
+}
 
-  cart.push({ productName, productPrice, quantity });
-  localStorage.setItem("cart", JSON.stringify(cart));
-  // alert(
-  //   `${productName} adicionado ao carrinho. Quantidade: ${quantity}, Total: R$ ${itemTotal
-  //     .toFixed(2)
-  //     .replace(".", ",")}`
-  // );
-  const phoneNumber = "5521979026552";
-  const message = `Eu quero ${quantity} ${productName}, a unidade saí R$${productPrice
-    .toFixed(2)
-    .replace(".", ",")}, e o total do pedido é R$${itemTotal
-    .toFixed(2)
-    .replace(".", ",")}.`;
-  const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+/**
+ * Função para efetuar logout do usuário
+ */
+function logout() {
+  localStorage.clear();
+  showUserInfo();
+  closeLoginPopup();
+  closeUserInfoPopup();
+}
+
+/**
+ * Função para adicionar item ao carrinho
+ */
+function addToCart(productName, price, quantityId) {
+  const quantity = parseInt(document.getElementById(quantityId).value);
+
+  const existingProduct = cart.find((item) => item.productName === productName);
+  if (existingProduct) {
+    existingProduct.quantity += quantity;
+  } else {
+    cart.push({ productName, price, quantity });
+  }
+
+  openCartPopup();
+  displayCart();
+}
+
+/**
+ * Função para exibir o carrinho
+ */
+function displayCart() {
+  const cartItems = document.getElementById("cart-items");
+  cartItems.innerHTML = "";
+  let total = 0;
+
+  cart.forEach((item) => {
+    cartItems.innerHTML += `<p>${item.productName} - R$ ${item.price} x ${item.quantity}</p>`;
+    total += item.price * item.quantity;
+  });
+
+  document.getElementById(
+    "cart-total"
+  ).textContent = `Total: R$ ${total.toFixed(2)}`;
+}
+
+/**
+ * Função para limpar o carrinho
+ */
+function clearCart() {
+  cart = [];
+  displayCart();
+}
+
+/**
+ * Função para abrir/fechar o pop-up do carrinho
+ */
+function toggleCart() {
+  const cartPopup = document.getElementById("cart-popup");
+  const cartToggleBtn = document.querySelector(".cart-toggle");
+
+  if (cartPopup.style.display === "none" || cartPopup.style.display === "") {
+    cartPopup.style.display = "block";
+    cartToggleBtn.textContent = "Minimizar Carrinho";
+  } else {
+    cartPopup.style.display = "none";
+    cartToggleBtn.textContent = "Maximizar Carrinho";
+  }
+}
+
+/**
+ * Função para abrir o pop-up do carrinho
+ */
+function openCartPopup() {
+  document.getElementById("cart-popup").style.display = "block";
+}
+
+/**
+ * Função para enviar o pedido via WhatsApp
+ */
+function sendOrder() {
+  const name = localStorage.getItem("name");
+  const address = localStorage.getItem("address");
+  const phone = localStorage.getItem("phone");
+
+  if (!name || !address || !phone) {
+    alert("Por favor, preencha Nome, Telefone e Endereço corretamente.");
+    return;
+  }
+
+  if (cart.length === 0) {
+    alert("Por favor, adicione ao menos um produto ao carrinho.");
+    return;
+  }
+
+  const observations = localStorage.getItem("observations");
+  let message = `Pedido de ${name} - ${address} - Tel: ${phone}:\n`;
+  let total = 0;
+
+  cart.forEach((item) => {
+    message += `${item.productName} - R$ ${item.price} x ${item.quantity}\n`;
+    total += item.price * item.quantity;
+  });
+
+  message += `\nTotal: R$ ${total.toFixed(2)}`;
+
+  if (observations) {
+    message += `\nObservações: ${observations}`;
+  }
+
+  const phoneNumber = "21979026552";
+  const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(
     message
   )}`;
-
   window.open(whatsappURL, "_blank");
 }
-function viewCart() {
-  window.open("./pages/cart.html", "_blank");
+
+/**
+ * Função para abrir o pop-up de login
+ */
+function openLoginPopup() {
+  document.getElementById("overlay").style.display = "block";
 }
+
+/**
+ * Função para fechar o pop-up de login
+ */
+function closeLoginPopup() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+/**
+ * Função para abrir o pop-up de informações do usuário
+ */
+function openUserInfoPopup() {
+  const userInfoPopup = document.getElementById("user-info-popup");
+  userInfoPopup.style.display = "block";
+
+  // Preencher o formulário com as informações do usuário
+  const user = getUserInfo();
+  if (user) {
+    document.getElementById("username").value = user.name;
+    document.getElementById("email").value = user.email;
+  }
+}
+
+/**
+ * Função para fechar o pop-up de informações do usuário
+ */
+function closeUserInfoPopup() {
+  document.getElementById("user-info-popup").style.display = "none";
+}
+
+/**
+ * Função para definir o estado de usuário conectado
+ */
+function setLoggedInState(user) {
+  document.getElementById("login-menu-item").style.display = "none";
+  document.getElementById("user-menu-item").style.display = "block";
+  document.getElementById("user-name").textContent = user.name;
+
+  // Salvar informações do usuário no localStorage
+  localStorage.setItem("user", JSON.stringify(user));
+}
+
+/**
+ * Função para definir o estado de usuário desconectado
+ */
+function setLoggedOutState() {
+  document.getElementById("login-menu-item").style.display = "block";
+  document.getElementById("user-menu-item").style.display = "none";
+
+  // Remover informações do usuário do localStorage
+  localStorage.removeItem("user");
+}
+
+/**
+ * Função para obter informações do usuário
+ */
+function getUserInfo() {
+  return JSON.parse(localStorage.getItem("user"));
+}
+
+// Verificar se o usuário está conectado ao carregar a página
+window.onload = function () {
+  const user = getUserInfo();
+  if (user) {
+    setLoggedInState(user);
+  }
+  showUserInfo();
+  populateLoginFields();
+};
+
+// Evento de submissão do formulário de informações do usuário
+document
+  .getElementById("user-info-form")
+  .addEventListener("submit", (event) => {
+    event.preventDefault();
+    // Lógica para salvar as informações do usuário
+    closeUserInfoPopup();
+  });
